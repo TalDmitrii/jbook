15c1972a0fb4f531f19f96b9973b49d6165cb337
TalDmitrii <30574780+TalDmitrii@users.noreply.github.com>
2023-03-18 13:18:49 +0300

    Use iframe for secure JS execution

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ import { fetchPlugin } from "./plugins/fetch-plugin";
 
 const App = () => {
     const ref = useRef<any>();
+    const iframe = useRef<any>();
     const [input, setInput] = useState("");
-    const [code, setCode] = useState("");
 
     const startService = async () => {
         ref.current = await esbuild.startService({
@@ -18,13 +18,15 @@ const App = () => {
 
     useEffect(() => {
         startService();
-        console.clear();
+        // console.clear();
     }, []);
 
     const onClickHandler = async () => {
         if (!ref.current) {
             return;
         }
+
+        iframe.current.srcdoc = html;
 
         const result = await ref.current.build({
             entryPoints: ["index.js"],
@@ -37,8 +39,29 @@ const App = () => {
             },
         });
 
-        setCode(result.outputFiles[0].text);
+        iframe.current.contentWindow.postMessage(
+            result.outputFiles[0].text,
+            "*"
+        );
     };
+
+    const html = `<html>
+        <head></head>
+        <body>
+            <div id="root"></div>
+            <script>
+                window.addEventListener("message", (event) => {
+                    try {
+                        eval(event.data);
+                    } catch(error) {
+                        const root = document.querySelector("#root");
+                        root.innerHTML = '<div>' + error + '</div>';
+                        throw error;
+                    }
+                }, false);
+            </script>
+        </body>
+    </html>`;
 
     return (
         <div>
@@ -51,7 +74,12 @@ const App = () => {
             <div>
                 <button onClick={onClickHandler}>SUBMIT</button>
             </div>
-            <pre>{code}</pre>
+            <iframe
+                ref={iframe}
+                srcDoc={html}
+                sandbox="allow-scripts"
+                title="Unique iframe"
+            ></iframe>
         </div>
     );
 };
