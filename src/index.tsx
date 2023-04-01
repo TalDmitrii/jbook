93d33a0a421a59827c365770945a66363b0d337a
TalDmitrii <30574780+TalDmitrii@users.noreply.github.com>
2023-04-01 13:38:03 +0300

    Create bundler helper

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import ReactDOM from "react-dom";
-import * as esbuild from "esbuild-wasm";
 
-import { unpkgPathPlugin } from "./plugins/unpcg-path-plug";
-import { fetchPlugin } from "./plugins/fetch-plugin";
+import { bundle } from "./bundler";
 
 import CodeEditor from "./components/code-editor";
 import Preview from "./components/preview";
@@ -11,39 +9,12 @@ import Preview from "./components/preview";
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 
 const App = () => {
-    const refESBuild = useRef<any>();
     const [code, setCode] = useState("");
     const [input, setInput] = useState("");
 
-    const startService = async () => {
-        refESBuild.current = await esbuild.startService({
-            worker: true,
-            wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
-        });
-    };
-
-    useEffect(() => {
-        startService();
-        console.clear();
-    }, []);
-
     const onClickHandler = async () => {
-        if (!refESBuild.current) {
-            return;
-        }
-
-        const result = await refESBuild.current.build({
-            entryPoints: ["index.js"],
-            bundle: true,
-            write: false,
-            plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-            define: {
-                "process.env.NODE_ENV": '"production"',
-                global: "window",
-            },
-        });
-
-        setCode(result.outputFiles[0].text);
+        const output = await bundle(input);
+        setCode(output);
     };
 
     return (
